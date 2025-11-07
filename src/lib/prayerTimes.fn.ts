0ba59fa0a30969c6f8510e666f80047c7db240b5
mhda7mn <230830129+mhda7mn@users.noreply.github.com>
2025-11-07 20:09:41 +0400

    chore: add prayer times function & types

---
+++ src/lib/prayerTimes.fn.ts
@@ -0,0 +1,23 @@
+import type { RootPrayerObject } from "../types/prayer.types";
+
+const fetchPrayerTimes = async (
+	lat: string,
+	long: string,
+	method: string
+) => {
+	const response = await fetch(
+		`https://islamicapi.com/api/v1/prayer-time/?lat=${lat}&lon=${long}&method=${method}&api_key=${
+			import.meta.env.API_KEY
+		}`
+	);
+
+	const data: RootPrayerObject = await response.json();
+
+	return {
+		prayerTimes: data.data.times,
+		date: data.data.date,
+		prohibitedTimes: data.data.prohibited_times,
+	};
+};
+
+export default fetchPrayerTimes;
---
+++ src/types/prayer.types.ts
@@ -0,0 +1,116 @@
+export interface RootPrayerObject {
+	code: number;
+	status: string;
+	data: Data;
+}
+
+interface Data {
+	times: Times;
+	date: Date;
+	qibla: Qibla;
+	prohibited_times: Prohibitedtimes;
+	timezone: Timezone;
+}
+
+interface Timezone {
+	name: string;
+	utc_offset: string;
+	abbreviation: string;
+}
+
+interface Prohibitedtimes {
+	sunrise: Sunrise;
+	noon: Sunrise;
+	sunset: Sunrise;
+}
+
+interface Sunrise {
+	start: string;
+	end: string;
+}
+
+interface Qibla {
+	direction: Direction;
+	distance: Distance;
+}
+
+interface Distance {
+	value: number;
+	unit: string;
+}
+
+interface Direction {
+	degrees: number;
+	from: string;
+	clockwise: boolean;
+}
+
+interface Date {
+	readable: string;
+	timestamp: string;
+	hijri: Hijri;
+	gregorian: Gregorian;
+}
+
+interface Gregorian {
+	date: string;
+	format: string;
+	day: string;
+	weekday: Weekday2;
+	month: Month2;
+	year: string;
+	designation: Designation;
+}
+
+interface Month2 {
+	number: number;
+	en: string;
+}
+
+interface Weekday2 {
+	en: string;
+}
+
+interface Hijri {
+	date: string;
+	format: string;
+	day: string;
+	weekday: Weekday;
+	month: Month;
+	year: string;
+	designation: Designation;
+	holidays: unknown[];
+	adjustedHolidays: unknown[];
+	method: string;
+}
+
+interface Designation {
+	abbreviated: string;
+	expanded: string;
+}
+
+interface Month {
+	number: number;
+	en: string;
+	ar: string;
+	days: number;
+}
+
+interface Weekday {
+	en: string;
+	ar: string;
+}
+
+interface Times {
+	Fajr: string;
+	Sunrise: string;
+	Dhuhr: string;
+	Asr: string;
+	Sunset: string;
+	Maghrib: string;
+	Isha: string;
+	Imsak: string;
+	Midnight: string;
+	Firstthird: string;
+	Lastthird: string;
+}
